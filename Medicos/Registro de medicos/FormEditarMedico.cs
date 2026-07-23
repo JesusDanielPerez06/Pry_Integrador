3fdc7318bf4092354ea1a1d3a8cb61ef530c3522
Daniel Medina <danielmediv@gmail.com>
2026-07-23 14:14:47 -0600

    Pullque pruebas CRUD editar

--- Medicos/Registro de medicos/FormEditarMedico.cs
+++ Medicos/Registro de medicos/FormEditarMedico.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using pry_integrador.Pruebas;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +14,162 @@ namespace pry_integrador.Medicos.Registro_de_medicos
 {
     public partial class FormEditarMedico : Form
     {
-        public FormEditarMedico()
+        private PruebaDataAcces conect;
+        private int idMedico;
+        
+        public FormEditarMedico(
+            int id,
+            string nombre,
+            string apellidoPaterno,
+            string apellidoMaterno,
+            string telefono,
+            string correo,
+            string cedula,
+            string especialdad)
         {
+
+            idMedico = id;
+
+
+            textNombreM.Text = nombre;
+            textApellidoP.Text = apellidoPaterno;
+            textApellidoM.Text = apellidoMaterno;
+            textTelefono.Text = telefono;
+            textMail.Text = correo;
+            textCedula.Text = cedula;
+            comboEspecialidad.Text = especialdad;
+             
             InitializeComponent();
         }
 
 
         private void btonGuardar_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(textNombreM.Text))
+            {
+                MessageBox.Show(
+                    "Campo obligatorio.",
+                    "Validación",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                textNombreM.Focus();
+                return;
+
+            }
+
+            if (string.IsNullOrWhiteSpace(textApellidoP.Text))
+            {
+                MessageBox.Show(
+                    "Campo obligatorio.",
+                    "Validación",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                textApellidoP.Focus();
+                return;
+
+            }
+
+            if (string.IsNullOrWhiteSpace(textApellidoM.Text))
+            {
+                MessageBox.Show(
+                    "Campo obligatorio.",
+                    "Validación",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                textApellidoM.Focus();
+                return;
+
+            }
+
+            if (string.IsNullOrWhiteSpace(textTelefono.Text))
+            {
+                MessageBox.Show(
+                    "Campo obligatorio.",
+                    "Validación",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                textTelefono.Focus();
+                return;
+
+            }
+
+            if (string.IsNullOrWhiteSpace(textMail.Text))
+            {
+                MessageBox.Show(
+                    "Campo obligatorio.",
+                    "Validación",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                textMail.Focus();
+                return;
+
+            }
+
+            if (string.IsNullOrWhiteSpace(textCedula.Text))
+            {
+                MessageBox.Show(
+                    "Campo obligatorio.",
+                    "Validación",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                textCedula.Focus();
+                return;
+
+            }
+
+            if (comboEspecialidad.SelectedIndex == -1)
+            {
+                MessageBox.Show(
+                    "Seleccione una especialidad.",
+                    "Validación",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                comboEspecialidad.Focus();
+                return;
+            }
+
+            conect = new PruebaDataAcces();
+            MySqlConnection conex = conect.GetConnection();
+
+            string consulta = "UPDATE medicos SET" +
+                "(nombre = @nombre, " +
+                "apellido_paterno = @apellidoPaterno, " +
+                "apellido_materno = @apellidoMaterno, " +
+                "telefono = @telefono, " +
+                "correo_electronico = @correo, " +
+                "cedula = @cedula, " +
+                "especialidad = @especialidad)";
+                
+
+            MySqlCommand comando = new MySqlCommand(consulta, conex);
+            comando.Parameters.AddWithValue("@nombre", textNombreM.Text);
+            comando.Parameters.AddWithValue("@apellidoPaterno", textApellidoP.Text);
+            comando.Parameters.AddWithValue("@apellidoMaterno", textApellidoM.Text);
+            comando.Parameters.AddWithValue("@telefono", textTelefono.Text);
+            comando.Parameters.AddWithValue("@correo", textMail.Text);
+            comando.Parameters.AddWithValue("@cedula", textCedula.Text);
+            comando.Parameters.AddWithValue("@especialidad", comboEspecialidad.Text);
+            comando.Parameters.AddWithValue("@idMedico", idMedico);
+
+            comando.ExecuteNonQuery();
+            conex.Close();
+
+            MessageBox.Show(
+                "Registro Exitoso.",
+                "Registro",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+
+            MessageBox.Show("Datos actualizados correctamente.");
+            Close();
 
         }
 
